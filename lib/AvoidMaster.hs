914f46cb35829b98d44e1e8eccb89976dfdbf77f
Joachim Fasting <joachim.fasting@gmail.com>
2012-09-23 16:33:02 +0200

    Add lib/AvoidMaster.hs

---
+++ lib/AvoidMaster.hs
@@ -0,0 +1,32 @@
+{-
+Prevent new windows from spawning in the master pane.
+Source: <http://ruderich.org/simon/config/xmonad>
+-}
+
+module XMonad.Action.AvoidMaster
+   ( manageFocus
+   , avoidMaster
+   ) where
+
+import XMonad.Core
+import XMonad.Hooks.ManageHelpers
+import XMonad.ManageHook
+import qualified XMonad.StackSet as W
+
+-- | Keep focus on master.
+manageFocus :: ManageHook
+manageFocus = composeOne
+    [
+      -- prevent new windows from spawning in the master pane
+      return True -?> doF avoidMaster
+      -- prevent windows moved to other workspaces to steal focus
+    , return True -?> doF W.focusDown
+    ]
+
+-- | Prevent windows from spawning in the master pane.
+avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
+avoidMaster = W.modify' $ \c -> case c of
+    W.Stack t [] (r:rs) -> W.Stack r [] (t:rs)
+    _                   -> c
+
+
