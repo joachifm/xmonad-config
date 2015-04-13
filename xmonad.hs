3a570e87ad0a8c9bfdd19dd0e51de220d18e9e95
joachifm <joachifm@fastmail.fm>
2015-04-13 23:49:47 +0200

    A minimal config for emacs-only work flow

--- xmonad.hs
+++ xmonad.hs
@@ -2,8 +2,6 @@
 
 module Main (main) where
 
-import XMonad.Actions.AvoidMaster
-
 import Data.Bits ((.|.))
 import qualified Data.Map as M
 import Graphics.X11.Xlib
@@ -17,20 +15,15 @@ import XMonad.Operations
 
 import XMonad.Actions.CopyWindow
 import XMonad.Actions.CycleWS
-import XMonad.Actions.FloatSnap
 import XMonad.Actions.GridSelect
 import XMonad.Actions.RotSlaves
 import XMonad.Actions.Warp
 import XMonad.Actions.WindowGo
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.NoBorders
-import XMonad.Layout.ShowWName
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.Place
-import XMonad.Prompt
-import XMonad.Prompt.Shell
 import XMonad.Util.Run (safeSpawn)
 
 ------------------------------------------------------------------------------
@@ -48,119 +41,75 @@ config = XMonad.defaultConfig
        , XMonad.layoutHook = layoutHook
        , XMonad.manageHook = manageHook
        , XMonad.workspaces = workspaces
+       , XMonad.startupHook = banish LowerRight
        }
 
 ------------------------------------------------------------------------------
 
-workspaces = [ "Work", "Term", "Web" ] ++ map show ([4..9]::[Int])
+workspaces = [ "Edit", "Web", "Mail", "IM" ] ++ map show [6..9]
 
 ------------------------------------------------------------------------------
 
 manageHook = composeAll [
-    checkDock                --> doIgnore
+    checkDock --> doIgnore
 
-    -- All clients except for dialogs submit to master
   , composeOne [ isDialog     -?> doFloat
                , isFullscreen -?> doFullFloat
-               , return True  -?> doF avoidMaster
                ]
 
     -- Assign clients to specific work spaces
   , composeOne [ className =? x -?> doShift w
-                 | (x, w) <- [ ("Emacs", "Work")
+                 | (x, w) <- [ ("Emacs", "Edit")
                              , ("Firefox", "Web")
+                             , ("Pidgin", "IM")
                              ]
                ]
 
     -- Floating clients
-  , composeOne [ className =? x -?> doFloat
-                 | x <- [ "mpv" ]
-               ]
+  , composeOne [ className =? x -?> doFloat | x <- floatingClients ]
   ]
 
-------------------------------------------------------------------------------
-
-layoutHook = modifiers layout
-    where
-        modifiers = showWName
-                  . layoutHints
-                  . avoidStruts
-                  . smartBorders
-
-        layout = Full ||| tall ||| wide
-
-        tall = Tall nmaster delta ratio
-        wide = Mirror tall
-
-        nmaster = 1
-        ratio   = 1/2
-        delta   = 3/100
+floatingClients = [ "mpv" ]
 
 ------------------------------------------------------------------------------
 
-keys conf@(XMonad.XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ ((modm .|. shiftMask, xK_Return), safeSpawn (XMonad.terminal conf) [])
-    , ((modm, xK_d), XMonad.spawn "(date '+%Y-%m-%d %T'; sleep 1) | dzen2")
-    , ((modm, xK_p), XMonad.spawn "dmenu_run")
-    , ((modm, xK_x), shellPrompt defaultXPConfig)
+layoutHook = smartBorders Full
 
-      -- Application hotkeys
-    , ((modm .|. controlMask, xK_l),
-       safeSpawn "xlock" [])
-    , ((modm .|. controlMask, xK_b),
-       runOrRaise "systemctl --user start firefox@joachifm" (className =? "Firefox"))
-    , ((modm .|. controlMask, xK_e),
-       runOrRaise "systemctl --user start emacs" (className =? "Emacs"))
+------------------------------------------------------------------------------
 
-    -- Window management
-    , ((modm, xK_w), goToSelected defaultGSConfig)
-    , ((modm, xK_s), windows copyToAll)
-    , ((modm .|. shiftMask, xK_s), killAllOtherCopies) -- undo copyToAll
-    , ((modm .|. shiftMask, xK_c), kill1)
-    , ((modm, xK_t), withFocused $ windows . W.sink)
+keys conf@(XMonad.XConfig {XMonad.modMask = modm}) = M.fromList $ [
+    ((modm .|. shiftMask, xK_Return), safeSpawn (XMonad.terminal conf) [])
 
-    -- Floating window movement
-    , ((modm .|. controlMask, xK_Left),
-       withFocused $ snapMove L Nothing)
-    , ((modm .|. controlMask, xK_Right),
-       withFocused $ snapMove R Nothing)
-    , ((modm .|. controlMask, xK_Up),
-       withFocused $ snapMove U Nothing)
-    , ((modm .|. controlMask, xK_Down),
-       withFocused $ snapMove D Nothing)
+  -- Window management
+  , ((modm, xK_w), goToSelected defaultGSConfig)
+  , ((modm .|. shiftMask, xK_c), kill1)
 
-    -- Workspaces
-    , ((modm, xK_h), moveTo Prev NonEmptyWS)
-    , ((modm, xK_l), moveTo Next NonEmptyWS)
-    , ((modm .|. shiftMask, xK_h), shiftToPrev >> prevWS)
-    , ((modm .|. shiftMask, xK_l), shiftToNext >> nextWS)
+  -- Workspaces
+  , ((modm, xK_h), moveTo Prev NonEmptyWS)
+  , ((modm, xK_l), moveTo Next NonEmptyWS)
 
-    -- Layouts
-    , ((modm, xK_space), sendMessage XMonad.NextLayout)
-    , ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
-    , ((modm, xK_n), refresh)
+  -- Window focus
+  , ((modm, xK_k), windows W.focusUp)
+  , ((modm, xK_j), windows W.focusDown)
+  , ((modm, xK_m), windows W.focusMaster)
 
-    -- Window focus
-    , ((modm, xK_k), windows W.focusUp)
-    , ((modm, xK_j), windows W.focusDown)
-    , ((modm, xK_m), windows W.focusMaster)
+  -- Master window
+  , ((modm, xK_Return), windows W.swapMaster)
+  , ((modm .|. shiftMask, xK_k), windows W.swapUp)
+  , ((modm .|. shiftMask, xK_j), windows W.swapDown)
 
-    -- Master window
-    , ((modm, xK_Return), windows W.swapMaster)
-    , ((modm .|. shiftMask, xK_k), windows W.swapUp)
-    , ((modm .|. shiftMask, xK_j), windows W.swapDown)
+  -- Slave windows
+  , ((modm .|. shiftMask, xK_Tab), rotSlavesUp)
 
-    -- Slave windows
-    , ((modm .|. shiftMask, xK_Tab), rotSlavesUp)
+  -- XMonad control
+  , ((modm .|. shiftMask, xK_q), XMonad.io exitSuccess)
+  , ((modm, xK_q), XMonad.spawn "xmonad --recompile && xmonad --restart")
 
-    -- XMonad control
-    , ((modm .|. shiftMask, xK_q), XMonad.io (exitWith ExitSuccess))
-    , ((modm, xK_q), XMonad.spawn "xmonad --recompile && xmonad --restart")
-
-    -- Mouse control
-    , ((modm, xK_b), banish LowerRight)
-    ]
-    ++
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+  -- Mouse control
+  , ((modm, xK_b), banish LowerRight)
+  ]
+  ++
+  [ ((m .|. modm, k), windows $ f i)
+      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+  ]
